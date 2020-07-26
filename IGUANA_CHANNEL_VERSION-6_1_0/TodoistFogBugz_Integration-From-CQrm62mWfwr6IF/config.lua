6ebe73114bf382ae0f1569e570c0104143449c27
admin <l.nguyen.paul@gmail.com>
2020-07-26 22:27:22 +0000

    Export from DigitalOcean Iguana: Move comment to below code

--- IGUANA_CHANNEL_VERSION-6_1_0/TodoistFogBugz_Integration-From-CQrm62mWfwr6IF/config.lua
+++ IGUANA_CHANNEL_VERSION-6_1_0/TodoistFogBugz_Integration-From-CQrm62mWfwr6IF/config.lua
@@ -1,5 +1,9 @@
 local file = require 'filesUtil'
 
+local config = json.parse{data=file.readFile(os.getenv('TODOIST_FOGBUGZ_INTEGRATION_CONFIG'))}
+
+return config
+
 -- Create a JSON file with the following structure and add in credentials and configurations
 -- Configure an environment variable called "TODOIST_FOGBUGZ_INTEGRATION_CONFIG" with the path of the JSON file
 --[[
@@ -24,7 +28,3 @@ local file = require 'filesUtil'
    }
 }
 ]]--
-
-local config = json.parse{data=file.readFile(os.getenv('TODOIST_FOGBUGZ_INTEGRATION_CONFIG'))}
-
-return config
